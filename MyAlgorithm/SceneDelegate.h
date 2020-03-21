a6c38016beceebfc689589d286752f38d2a7e82d
shizidy <shizizuo1990>
2020-03-21 10:49:49 +0800

    Initial Commit

---
+++ MyAlgorithm/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MyAlgorithm
+//
+//  Created by wdyzmx on 2020/3/21.
+//  Copyright © 2020 wdyzmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ MyAlgorithm/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyAlgorithm
+//
+//  Created by wdyzmx on 2020/3/21.
+//  Copyright © 2020 wdyzmx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
